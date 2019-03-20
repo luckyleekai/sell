bde221ffff6833cd7aef1c77a1746e3fdf83ee70
kevinandleekai <zhizhen23@sina.com>
2019-03-20 19:20:40 +0800

    menu and food list init

--- src/common/components/icon/Icon.vue
+++ src/common/components/icon/Icon.vue
@@ -1,7 +1,5 @@
 <template>
-  <span class="icon" :class="fullClass">
-    hello world
-  </span>
+  <span class="icon" :class="[fullClass, uClass]"></span>
 </template>
 
 <script>
@@ -13,8 +11,12 @@ export default {
       default: 0
     },
     iconSize: {
-      type: Number,
+      type: [Number, String],
       default: 12
+    },
+    uClass: {
+      type: String,
+      default: ''
     }
   },
   computed: {
@@ -22,9 +24,6 @@ export default {
       let classMap = ['decrese', 'discount', 'special', 'invoice', 'guarantee']
       return `icon${this.iconSize} ${classMap[this.iconType]}`
     }
-  },
-  created() {
-    console.log(this.iconSize)
   }
 }
 </script>
@@ -50,6 +49,21 @@ export default {
       bg-image('invoice_1')
     &.special
       bg-image('special_1')
+  &.icon12_specail
+    width 12px
+    height 12px
+    background-size 12px 12px
+    background-repeat no-repeat
+    &.decrese
+      bg-image('decrease_3')
+    &.discount
+      bg-image('discount_3')
+    &.guarantee
+      bg-image('guarantee_3')
+    &.invoice
+      bg-image('invoice_3')
+    &.special
+      bg-image('special_3')
   &.icon16
     width 16px
     height 16px
--- src/components/home/components/Goods.vue
+++ src/components/home/components/Goods.vue
@@ -1,12 +1,58 @@
 <template>
   <div class="goods">
-    <div class="menu-box"></div>
-    <div class="goods-box"></div>
+    <div class="menu-box" ref="menuBox">
+      <ul>
+        <li class="menu-item"
+            v-for="(menuItem, index) in goods"
+            :key="index"
+        >
+          <div class="menu-item-detail">
+            <My-Icon v-if="menuItem.type > 0"
+              :iconType="menuItem.type"
+              iconSize="12_specail"
+              class="menu-icon"
+            />
+            <span class="menu-text">{{ menuItem.name }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="goods-box" ref="goodsBox">
+      <ul>
+        <li v-for="(item, index) in goods"
+            :key="index"
+            class="goods-list">
+          <h2 class="goods-list-title">{{item.name}}</h2>
+          <ul class="goods-item-box">
+            <li v-for="(food, index) in item.foods"
+                :key="index"
+                class="goods-item">
+              <div class="goods-image-box">
+                <img :src="food.icon" :alt="food.name" class="goods-image">
+              </div>
+              <div class="goods-content-box">
+                <h3 class="goods-content-title">{{food.name}}</h3>
+                <p class="goods-desc">{{food.description}}</p>
+                <div class="goods-extra">
+                  <span class="goods-sell-count">月售{{food.sellCount}}份</span>
+                  <span>好評率{{food.rating}}</span>
+                </div>
+                <div class="goods-price">
+                  <span class="goods-price-now">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="goods-price-old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import MyIcon from '@/common/components/icon/Icon'
 
 export default {
   name: 'goods',
@@ -15,14 +61,26 @@ export default {
       goods: []
     }
   },
+  computed: {
+  },
   created() {
     axios.get('/mock/data.json').then((res) => {
       this.goods = res.data.goods
-      console.log(this.goods)
     }, (err) => {
       console.log('请求出错')
       console.log(err)
     })
+  },
+  components: {
+    MyIcon
+  },
+  methods: {
+    initFoodList() {
+      let menuBox = this.$refs['menuBox']
+      this.menuList = new BScroll(menuBox, {})
+      let goodsBox = this.$refs['goodsBox']
+      this.foodList = new BScroll(goodsBox, {})
+    }
   }
 }
 </script>
@@ -38,6 +96,71 @@ export default {
   .menu-box
     width 80px
     background-color #f3f5f7
+    .menu-item
+      width 56px
+      height 56px
+      line-height 14px
+      display table
+      padding 0 12px
+    .menu-item-detail
+      display table-cell
+      vertical-align middle
+      font-size 0
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    .menu-icon
+      margin-right 2px
+    .menu-text
+      font-size 14px
   .goods-box
     flex 1
+    // .goods-list
+    .goods-list-title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background-color #f3f5f7
+    .goods-item-box
+      overflow hidden
+    .goods-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      &:last-child
+        border-width 0
+        padding-bottom 0
+    .goods-image-box
+      width 57px
+      margin-right 10px
+    .goods-content-box
+      flex 1
+    .goods-content-title
+      margin 2px 0 8px
+      font-size 14px
+      height 14px
+      line-height 14px
+      color rgb(7, 17, 27)
+    .goods-desc,.goods-extra
+      font-size 10px
+      line-height 10px
+      color rgb(147, 153, 159)
+    .goods-desc
+      margin-bottom 8px
+    .goods-sell-count
+      margin-right 12px
+    .goods-price
+      font-size 0
+      font-weight 700
+      line-height 24px
+    .goods-price-now
+      margin-right 8px
+      font-size 14px
+      color rgb(240, 20, 20)
+    .goods-price-old
+      text-decoration line-through
+      font-size 10px
+      color rgb(147, 153, 159)
 </style>
